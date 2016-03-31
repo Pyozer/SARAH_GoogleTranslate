a7a3ea5cfdd1242048ea3c6fb20843dea56b15c4
Jean-Charles Moussé <jeancharles.msse@gmail.com>
2016-03-31 22:00:54 +0200

    First Release

---
+++ GoogleTranslate/GoogleTranslate.js
@@ -0,0 +1,104 @@
+var token;
+var interval = 100;		// msec
+var repeat = 50;		// n x interval = timeout ... 50 x 100 = 5000 msec de timeout ...
+var cnt = 0;
+var cpt_initial;
+
+exports.init = function () {
+    info('[ GoogleTranslate ] is initializing ...');
+}
+
+exports.action = function(data, callback){
+
+	// valeur initiale du compteur ...
+	cpt_initial = SARAH.context.SpeechReco.compteur;
+	cnt = 0;
+	token = setInterval(function() {
+		checkSpeechReco(SARAH, callback, data)
+	}, interval);
+}
+
+function checkSpeechReco(SARAH, callback, data) {
+	var new_cpt = SARAH.context.SpeechReco.compteur;
+	
+	if (new_cpt != cpt_initial) {
+
+		var search = SARAH.context.SpeechReco.lastReco;
+		console.log ("Search: " + search);
+		// On prend pas en compte la langue ici pour éviter d'avoir un regex pour chaque langues du xml
+		var rgxp = /(traduit|traduis|traduire|tu peux traduire|donne moi le mot|comment on dit|peux tu me traduire) (.+) en (.+)/i;
+		// on s'assure que Google a bien compris
+		var match = search.trim().match(rgxp);
+		if (!match || match.length <= 1){
+			console.log("FAIL");
+			clearInterval(token);
+			return callback({ 'tts': "Je ne comprends pas" });
+		}
+		// on peut maintenant s'occuper du/des mots qui sont recherchés
+		search = match[2];
+		clearInterval(token);
+		console.log("Cnt: " + cnt);
+		return getTraduction(search, callback, data.lang);
+	} else {
+		cnt += interval;
+		if (cnt > (interval * repeat)) {
+			callback({'tts': "Google Chrome n'a pas répondu assez vite"});
+			clearInterval(token);
+			return;
+		}
+	}
+}
+
+var getTraduction = function(search, callback, lang) {
+	search = search.trim();
+	search = search.toLowerCase();
+
+	switch(lang) {
+		case 'en':
+			var language = "Anglais";
+		break;
+		case 'es':
+			var language = "Espagnol";
+		break;
+		case 'de':
+			var language = "Allemand";
+		break;
+		case 'zh-CN':
+			var language = "Chinois";
+		break;
+		case 'la':
+			var language = "Latin";
+		break;
+		case 'ru':
+			var language = "Russe";
+		break;
+		case 'ro':
+			var language = "Roumain";
+		break;
+		case 'fr':
+			var language = "Français";
+		break;
+		default:
+			var language = "Français";
+		break;
+	}
+
+	var nomcherchercomplet = 'https://translate.google.com/?q=' + search + '&sl=auto&tl=' + lang + '#auto/' + lang + '/' + search;
+
+	var request = require('request');
+	var cheerio = require('cheerio');
+
+    var url = nomcherchercomplet;
+    request({ 'uri': url, 'headers': { 'Accept-Charset': 'utf-8' }, 'encoding': 'binary' }, function(error, response, html) {
+
+        var $ = cheerio.load(html, { xmlMode: false, ignoreWhitespace: false, lowerCaseTags: false });
+
+        var resultat = $('#gt-res-c #gt-res-content span#result_box').text().trim();
+
+        console.log('=[ Traduction de ' + search + ' en ' + language + ' ]=');
+        console.log('Résultat: ' + resultat);
+
+        callback({ 'tts': search + " en " + language + " se dit " + resultat });
+        return;
+    });
+}
---
+++ readme.md
@@ -0,0 +1,27 @@
+﻿Plugin Traduction pour S.A.R.A.H
+================================================================================
+
+Le plugin nécessite tout d'abord SpeechReco (similaire à Scribe).
+
+Que vous pouvez trouver ici: https://github.com/tilleul/Sarah.dictation.v2
+
+Vous pouvez toujours modifier le plugin pour l'adapter au système habitutel pour le Garbage ;)
+
+Prérequis
+---------
+- Sarah v4
+- Le plugin SpeechReco
+- Et ce plugin ;)
+
+Installation
+------------
+- Copier le dossier GoogleTranslate dans votre dossier "plugins" de Sarah
+- Remplacez Jarvis dans GoogleTranslate.xml par celui que vous utilisez (ex: Sarah, ...)
+
+Comment ça marche ?
+--------------------------
+- Démarrez S.A.R.A.H (ainsi que le serveur https du plugin SpeechReco)
+- Allez sur la page https://127.0.0.1:4300 pour avoir la reconnaissance du Garbage via Google Chrome
+- Vous n'avez plus qu'a demander par exemple: "Jarvis traduit bonjour en anglais"
+- Vous pouvez demander n'importe quel mot ou phrase dans n'importe quel langue (sous réserve quelle soit indiqué dans le xml et ajouter dans le .js).
+- En faite, le plugin récupère seulement la phrase demandé, ainsi que la langue cible, et récupère le résultat de Google Translate.
